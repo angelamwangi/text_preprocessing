2901d7f2e67ccde3ad26ecc00be06a3727a77980
angelaamm <angela.m.mwangi@gmail.com>
2026-04-09 12:38:23 +0300

    Initial commit for text preprocessing project

---
+++ language_model.py
@@ -0,0 +1,13 @@
+#trying to build a model which knows your language
+#N-gram
+#input i am the king of----- ouptut=jungle
+#N-gram= a sequence of n words
+#|i|am|the|king|=unigram
+#|i am|the king|=bigram
+#|i am the|=trigram
+
+#unigram formula
+#(P)=P(w)=count(w)/n
+#count(w)=count of words  n=no of words
+
+
